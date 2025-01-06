c9fa2febca0dd28fff593121a7726496d8e5f58c
Joshua <joshuaa@connect.hku.hk>
2025-01-06 21:30:48 +0800

    refactor: move logic into new user service

--- src/main/java/com/joshua/application/UserController.java
+++ src/main/java/com/joshua/application/UserController.java
@@ -1,28 +1,30 @@
 package com.joshua.application;
 
+import lombok.RequiredArgsConstructor;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 
 @RestController
+@RequiredArgsConstructor
 public class UserController {
 
 
-    private List<User> userList = new ArrayList<>();
+    private final UserService userService;
     @GetMapping("/api/users")
     public List<User> getAllUsers(){
-        return userList;
+        return userService.fetchAllUsers();
     }
 
     @PostMapping("/api/users")
-    public List<User> createUser(@RequestBody User user){
-        userList.add(user);
-        return userList;
+    public String createUser(@RequestBody User user){userService.addUser(user);
+      return "User added successfully";
+
     }
 
 }
