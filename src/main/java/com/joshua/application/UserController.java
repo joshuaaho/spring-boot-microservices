81bd97603d4bfd62569ba9c4e105d8e4201c66da
Joshua <joshuaa@connect.hku.hk>
2025-01-06 21:11:23 +0800

    feat: add get all users and post user functionality

--- src/main/java/com/joshua/application/UserController.java
+++ src/main/java/com/joshua/application/UserController.java
@@ -1,2 +1,28 @@
-package com.joshua.application;public class UserController {
+package com.joshua.application;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@RestController
+public class UserController {
+
+
+    private List<User> userList = new ArrayList<>();
+    @GetMapping("/api/users")
+    public List<User> getAllUsers(){
+        return userList;
+    }
+
+    @PostMapping("/api/users")
+    public List<User> createUser(@RequestBody User user){
+        userList.add(user);
+        return userList;
+    }
+
 }
