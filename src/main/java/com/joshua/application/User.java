b7df8b92c55fb829438c59eb8a791515ffe02a0c
Joshua <joshuaa@connect.hku.hk>
2025-01-07 13:21:11 +0800

    feat: add more user properties

--- src/main/java/com/joshua/application/User.java
+++ src/main/java/com/joshua/application/User.java
@@ -21,6 +21,9 @@ public class User {
     private Long id;
     private String firstName;
     private String lastName;
+    private String email;
+    private String phone;
+    private UserRole role = UserRole.CUSTOMER;
 
 
 
---
+++ src/main/java/com/joshua/application/UserRole.java
@@ -0,0 +1,5 @@
+package com.joshua.application;
+
+public enum UserRole {
+    CUSTOMER,ADMIN
+}
