ed384a2eb61c68225a2d9b15813de06f1fbb9989
Joshua <joshuaa@connect.hku.hk>
2025-01-10 15:03:15 +0800

    feat: add address entity and relationship with user

---
+++ src/main/java/com/joshua/application/model/Address.java
@@ -0,0 +1,19 @@
+package com.joshua.application.model;
+
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "addresses")
+public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String street;
+    private String city;
+    private String state;
+    private String country;
+    private String zipcode;
+
+
+}
--- src/main/java/com/joshua/application/model/User.java
+++ src/main/java/com/joshua/application/model/User.java
@@ -1,12 +1,14 @@
 package com.joshua.application.model;
 
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
-import jakarta.persistence.Entity;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Data
 //@Entity(name = "user_table")
@@ -24,6 +26,16 @@ public class User {
     private String phone;
     private UserRole role = UserRole.CUSTOMER;
 
+    @OneToOne(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "address_id", referencedColumnName = "id")
+    private Address address;
+
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
 
 
 
